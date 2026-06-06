93dcd003cd984905cb5109dea31022a7b29b7c56
mkuciapki <m.kuciapski@gmail.com>
2026-06-06 09:31:50 +0200

    initial

--- app/competences/page.tsx
+++ app/competences/page.tsx
@@ -1,7 +1,8 @@
-export default function Home() {
+import competences from "../assets/competences";
+export default function Competences() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      index
+      competences
     </div>
   );
 }
